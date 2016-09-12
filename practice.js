2058f7cfc199795b3d8cb6c2a5d09497f67a74a5
Sarah Veness <sarahhveness@gmail.com>
2016-09-12 19:55:41 +0000

    used node.js streams to return HTML of example.com

---
+++ practice.js
@@ -0,0 +1,20 @@
+var http = require("http");
+
+var printExampleHTML = {
+  host: "example.com",
+  path: "/"
+};
+
+http.get(printExampleHTML, (response) => {
+
+  response.setEncoding("utf8");
+
+  response.on("data", function(data) {
+    console.log(data);
+  });
+
+  response.on("end", function() {
+    console.log("If you see this, you did it right. ");
+  });
+
+})
