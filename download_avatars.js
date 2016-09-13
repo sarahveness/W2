e9b645d6ede144049dee9912e6540ca414a33a23
Sarah Veness <sarahhveness@gmail.com>
2016-09-13 04:35:11 +0000

    avatars added to avatars folder

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,9 @@
+require('dotenv').config();
+var fs = require('fs');
+
 var request = require("request");
+
+var api_token = process.env['GITHUB_API_TOKEN'];
 
 var inputs = process.argv.slice(2);
 
@@ -12,18 +17,44 @@ function getRepoContributors(repoOwner, repoName, cb) {
   result = [];
   request.get({
     url: apiRoot + '/repos/' + repoOwner + "/" + repoName + '/contributors',
+    auth: {
+      user: 'sarahveness',
+      pass: api_token
+    },
     headers: {
       'User-Agent': 'Lighthouse'
     },
     json: true
   }, function (err, incomingMessage, responseBody) {
-    if (err) {
-      console.log(err);
-      return;
+      if (err) {
+        console.log(err);
+        return;
+      }
+      var value = 0;
+      var index = responseBody.length;
+
+
+     responseBody.forEach(function(value, index) {
+         var filename = "./avatars/"+value.id+".jpg";
+         downloadImageByURL(value.avatar_url, filename);
+       });
+
+
+
+    });
+
+
+    function downloadImageByURL(url, filePath) {
+      request
+      .get(url)
+      .on('error', function(err) {
+        console.log(err)
+      })
+      .pipe(fs.createWriteStream(filePath))
     }
-    console.log(responseBody);
-  });
 }
+
+
 
 getRepoContributors(owner, repo, (err, result) => {
   console.log("Errors:", err);
@@ -51,3 +82,4 @@ getRepoContributors(owner, repo, (err, result) => {
 //   }
 //   console.log(responseBody);
 // });
+
