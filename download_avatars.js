f449ec947a68149f8d0e4783f86880ff56df124d
Sarah Veness <sarahhveness@gmail.com>
2016-09-13 01:30:43 +0000

    implement js function to handle request

--- download_avatars.js
+++ download_avatars.js
@@ -1,33 +1,53 @@
-const request = require("request");
+var request = require("request");
 
 var inputs = process.argv.slice(2);
 
 var owner = inputs[0];
 var repo = inputs[1];
 var contributors = [];
-var apiRoot = 'https://api.github.com';
-var url = apiRoot + '/repos/' + owner + '/' + repo + '/contributors';
-
-console.log(url);
-
-request.get({
-  url: url,
-  headers: {
-    'User-Agent': 'Lighthouse'
-  },
-  json: true
-}, function (err, incomingMessage, responseBody) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(responseBody);
-});
+var apiRoot = "https://api.github.com";
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    var repoOwner = owner;
-    var repoName = repo;
+  result = [];
+  request.get({
+    url: apiRoot + '/repos/' + repoOwner + "/" + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'Lighthouse'
+    },
+    json: true
+  }, function (err, incomingMessage, responseBody) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(responseBody);
+  });
 }
 
-// getRepoContributors()
+getRepoContributors(owner, repo, (err, result) => {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
+
+
+// ——— GITHUB API ENDPOINT STEP ———
+// var url = apiRoot + '/repos/' + owner + "/" + repo + '/contributors';
+
+// console.log(url);
+
+// request.get({
+//   url: url,
+//   headers: {
+//     'User-Agent': 'Lighthouse'
+//   },
+//   json: true
+// }, function (err, incomingMessage, responseBody) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(responseBody);
+// });
