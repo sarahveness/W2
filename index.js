1562060a74d2423cdbfd4cfb758e28e0ea88c69b
Sarah Veness <sarahhveness@gmail.com>
2016-09-14 01:44:08 +0000

    Add index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+var fs = require('fs');
+
+var request = require("request");
+
+var api_token = process.env['GITHUB_API_TOKEN'];
+
+var inputs = process.argv.slice(2);
+
+var owner = inputs[0];
+var repo = inputs[1];
+var apiRoot = "https://api.github.com";
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  result = [];
+  request.get({
+    url: apiRoot + '/repos/' + repoOwner + "/" + repoName + '/contributors',
+    auth: {
+      user: 'sarahveness',
+      pass: api_token
+    },
+    headers: {
+      'User-Agent': 'Lighthouse'
+    },
+    json: true
+  }, function (err, incomingMessage, responseBody) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      var value = 0;
+      var index = responseBody.length;
+
+
+     responseBody.forEach(function(value, index) {
+         var filename = "./avatars/"+value.id+".jpg";
+         downloadImageByURL(value.avatar_url, filename);
+       });
+
+
+
+    });
+
+
+    function downloadImageByURL(url, filePath) {
+      request
+      .get(url)
+      .on('error', function(err) {
+        console.log(err)
+      })
+      .pipe(fs.createWriteStream(filePath))
+    }
+}
+
+
+
+getRepoContributors(owner, repo, (err, result) => {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
